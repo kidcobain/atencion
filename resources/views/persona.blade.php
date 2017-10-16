d54625dd8d283de7e0199ac652fda57365506bc2
antonio marcano <kidcobain@hotmail.com>
2017-10-16 04:16:38 -0400

    lab with modals

--- resources/views/persona.blade.php
+++ resources/views/persona.blade.php
@@ -2,6 +2,29 @@
 
 @section('content')
 <div class="container">
+    <form id="frmEliminarUsuario" action="" method="POST">
+        <input type="hidden" id="idusuario" name="idusuario" value="">
+        <input type="hidden" id="opcion" name="opcion" value="eliminar">
+        <!-- Modal -->
+        <div class="modal fade" id="modalEliminar" tabindex="-1" role="dialog" aria-labelledby="modalEliminarLabel">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="modalEliminarLabel">Eliminar Usuario</h4>
+                    </div>
+                    <div class="modal-body">                            
+                        ¿Está seguro de eliminar al usuario?<strong data-name=""></strong>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" id="eliminar-usuario" class="btn btn-primary" data-dismiss="modal">Aceptar</button>
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Modal -->
+    </form>
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -140,7 +163,7 @@
                         </td>
                       <td>
                         <a href="/solicitud/{{$solicitud->id}}/edicion" class="button tiny radius secondary">MODIFICAR</a>
-                        <a href="/solicitud/{{$solicitud->id}}/eliminar" class="button tiny radius alert">ELIMINAR</a>
+                        <a href="/solicitud/{{$solicitud->id}}/eliminar" class="button tiny radius alert" data-toggle='modal' data-target='#modalEliminar'>ELIMINAR</a>
                       </td>
                     </tr>
                     @endforeach
